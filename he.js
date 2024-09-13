bce8b3afc835cbd4c9400697dbdde58cd804427d
itsabhay1 <abhayag41@gmail.com>
2024-09-13 23:38:46 +0530

    Initial commit

---
+++ he.js
@@ -0,0 +1,87 @@
+// console.log("hello world")
+// // alert("hello");
+// let x = 5;
+// console.log(x);
+// let o = {name: "hi", salary: "1000crore"
+// };
+// console.log(o)
+
+// let a = 1;
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+    
+// }
+
+// const func1 = (x)=>{
+//     console.log("I am a arrow functionf",x)
+// }
+
+// func1(45);
+
+// let his = "heloo"
+// let hname = "mine"
+// console.log(`${his}, This is ${hname}`)
+
+// arr = [1,2,3,4,5,6,7,8]
+// console.log(arr)
+
+// let a1 = [1,2,3,4]
+// let a2 = [5,6]
+// let a3 = [7,8,9]
+
+// let a4 = [6,4,7,9,2,5,1,0,,3,7,8,4,2]
+
+// let a = [4.6,234,545,34,52,89,26]
+
+// for (let index = 0; index < a.length; index++) {
+//     const element = a[index];
+//     console.log(element)
+    
+
+// a.forEach((value,index arr) => {
+//     console.log(value,index,arr)
+// });
+
+// let obj = {
+//     a : 1,
+//     b : 2,
+//     c : 3
+// } 
+
+// for (const key in obj) {
+//     if (Object.prototype.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key,element)
+        
+//     }
+// }
+
+// let a = [2,46,89,336,,7,56,53,5]
+
+// for (const element of a) {
+//     console.log(element)
+// }
+
+
+arr = [1,4,6,9,11,25]
+// narr = []
+// for (const key in arr) {
+//     if (Object.prototype.hasOwnProperty.call(arr, key)) {
+//         const element = arr[key];
+//         narr.push(element**2)
+//     }
+// }
+// console.log(narr)
+
+// let narr = arr.map((e)=>{
+//     return e**2
+// })
+// console.log(narr)
+
+// let someno = 33
+// let nst = String(someno)
+// console.log(nst);
+// console.log(typeof nst);
+
+
+console.log(undefined>=0);
---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+// Add event listeners to navigation links
+document.querySelectorAll('nav a').forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        // Add logic to handle navigation clicks
+    });
+});
